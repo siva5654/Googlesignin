a487b6c73672a25c570340b0232c50304eeef7da
SIVAKRISHNA <44694040+siva5654@users.noreply.github.com>
2021-06-13 18:56:43 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,6 +7,13 @@ $(document).ready(function(){
 	getTime();
 	
 })
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
 let getTime =() =>{
 	let date = new Date().toLocaleString();
 	$('#time').text(date);
