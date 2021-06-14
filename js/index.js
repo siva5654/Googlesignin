9b453fb44ecdca1976c96aa21add88afab592b60
SIVAKRISHNA <44694040+siva5654@users.noreply.github.com>
2021-06-14 17:45:54 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -37,18 +37,7 @@ let onSignIn =(googleUser) => {
 	console.log(profile);
 }
 
-let signOut =() =>{
-	var auth2 = gapi.auth2.getAuthInstance();
-   	 auth2.signOut().then(function () {
-    	localStorage.removeItem("userinfo");
-    	  let goForm = document.querySelector('.g-signin2');
-		  let  userInfo = document.querySelector('#userinfo');
-		  // goForm.classList.remove('none')
-		  // userInfo.classList.remove('block');
 
-      console.log('User signed out.');
-    });
-}
 
 
 let getTime =() =>{
