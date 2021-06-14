229da9c9322f1498abab4b589631b6c362d5a05d
SIVAKRISHNA <44694040+siva5654@users.noreply.github.com>
2021-06-14 17:29:54 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -13,6 +13,14 @@ let cookieSsEna = () =>{
 	console.log(x)
 
 }
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
+
 function onSignIn(googleUser) {
 	
   var profile = googleUser.getBasicProfile();
